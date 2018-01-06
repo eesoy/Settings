c5f2a8aad25251e78fb9488eb47b0bfa0c706017
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-06 15:41:56 +0900

    version Check

--- Settings/ViewController.swift
+++ Settings/ViewController.swift
@@ -12,14 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let savedVer = UserDefaults.standard.value(forKey: "version") as? String {
+            //최초 실행 아님
+            if let currentVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String {
+                print("현재 앱 버전: \(currentVersion)")
+                if savedVer != currentVersion {
+                    print("업데이트 후 최초 실행, 앱 버전: \(currentVersion)")
+                    UserDefaults.standard.set(currentVersion, forKey: "version") //현재 버전 저장
+                }
+            }
+        } else {
+            //최초 실행일 경우 현재 앱 버전을 가져와서 저장
+            //get info.plist files - apps에 올라가는 버전값
+            if let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String {
+                print("최초 실행, 앱 버전: \(version)")
+                UserDefaults.standard.set(version, forKey: "version")
+            }
+        }
+        
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
